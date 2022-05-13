731039043b376e94e7fc56d1cb2106df55f896e1
Crazedpotato <92868941+THECRAZEDPOTATTO@users.noreply.github.com>
2022-05-13 11:44:48 -0400

    Update main.sh

--- linux/main.sh
+++ linux/main.sh
@@ -3,3 +3,12 @@ sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328
 echo "deb https://download.mono-project.com/repo/ubuntu stable-focal main" | sudo tee /etc/apt/sources.list.d/mono-official-stable.list
 sudo apt update
 sudo apt install mono-devel
+sudo apt install g++
+sudo apt install build-essential
+sudo apt install default-jre
+javac -version
+sudo apt-get install ruby-full
+python3 --version
+echo 'If python is not installed run this: sudo apt-get install python3'
+curl -L http://xrl.us/installperlnix | bash
+sudo apt-get install sbcl

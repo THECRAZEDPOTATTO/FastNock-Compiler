3fdc75d36de1fdcdd998967047c9949a992587ee
Crazedpotato <92868941+THECRAZEDPOTATTO@users.noreply.github.com>
2022-05-14 17:10:31 -0400

    Update main.sh

--- linux/main.sh
+++ linux/main.sh
@@ -8,7 +8,7 @@ echo compiling $filename...
 case $filetype in  
   cs|CS|Csharp|csharp|.cs) mono $filename.cs;; 
   cpp|c++|cplusplus|CCC|.cpp|CPP) g++ $filename.cpp ;; 
-  c|C|.c) g++ $filename.c ;; 
+  c|C|.c) gcc $filename.c ;; 
   ruby|rs|gem|gemfile|rb) ocra $filename.rb;;
   ps1|ps|powershell|.ps1) echo linux does not support powershell or Batch ;; 
   bat|batch|.bat) echo linux does not support powershell or Batch ;; 
